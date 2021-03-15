86c086c3a34267f2d5f49a4ab4fb3b6591dd7234
OliviaZhang2198 <yz4229@nyu.edu>
2021-03-14 20:22:35 -0400

    ClassInfo page

--- front-end/src/ClassInfo.js
+++ front-end/src/ClassInfo.js
@@ -1,7 +1,40 @@
-
+import React from'react';
+import ReactDOM from 'react-dom';
+import MenuBar from './MenuBar';
+import './ClassInfo.css';
+import './MenuBar.css';
+import ProfInfo from'./ProfInfo';
+import {Link,Switch} from 'react-router-dom';
 
 function ClassInfo(props){
-    return
+    
+    return(
+
+        <div className="classinfo">
+            <MenuBar>
+            </MenuBar>
+            <p>
+                Class Name
+            </p>
+            <p className="time-place">
+                Time and Place
+            </p>
+            <p className="intro">
+                Brief Introduction of the Class
+            </p>
+            <p className="prereq">
+                Prerequisites
+            </p>
+            <p className="prof">
+                <Link to = "/ProfInfo">
+                    Professor Information
+                </Link>
+            </p>
+            
+
+        </div>
+
+    )
 }
 
 
