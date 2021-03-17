ae56a865147ecd1d6a878621fac88059c71631c2
EvlZhang <70420985+EvlZhang@users.noreply.github.com>
2021-03-17 12:40:48 -0400

    Dropdown foundations

--- front-end/src/Dropdown.js
+++ front-end/src/Dropdown.js
@@ -4,9 +4,15 @@ import {Link} from 'react-router-dom'
 
 const Dropdown=(props)=>{
     return(
-        <nav>
-            <Link to="/">Home</Link>
-            {/* </nav><Link to="/"  */}
-        </nav>
+        <div className="dd-wrapper">
+            <div className="dd-header">
+                <div className="dd-header-title"></div>
+            </div>
+            <div className="dd-list">
+                <button className="dd-list-item"></button>
+                <button className="dd-list-item"></button>
+                <button className="dd-list-item"></button>
+            </div>
+</div>
     )
 }
