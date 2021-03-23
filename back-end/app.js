f536edfed248facbc4e986baf2680feb6ac47d89
EvlZhang <70420985+EvlZhang@users.noreply.github.com>
2021-03-23 12:03:44 -0400

    installed package

--- back-end/app.js
+++ back-end/app.js
@@ -3,4 +3,8 @@ const express = require("express") // CommonJS import style!
 const app = express() // instantiate an Express object
 // we will put some server logic here later...
 // export the express app we created to make it available to other modules
+app.get("/", (req, res) => {
+    res.send("Hello!")
+  })
+  
 module.exports = app
