a989fc76432dcb12168192bef321158777df2aa5
evlzhang <zyh991001@gmail.com>
2021-04-01 14:50:12 -0400

    eslint style fix

--- back-end/app.js
+++ back-end/app.js
@@ -1,156 +1,155 @@
 // import and instantiate express
-const express = require("express") // CommonJS import style!
-const app = express() // instantiate an Express object
+const express = require('express'); // CommonJS import style!
+const app = express(); // instantiate an Express object
 // we will put some server logic here later...
 // export the express app we created to make it available to other modules
 
-const multer =require("multer")
-const axios = require("axios")
-const morgan=require("morgan")
-const scraper=require("./scraper")
-const mocha=require("mocha")
-const chai=require("chai")
+const multer =require('multer');
+const axios = require('axios');
+const morgan=require('morgan');
+const scraper=require('./scraper');
+const mocha=require('mocha');
+const chai=require('chai');
 
 
-var cors = require('cors')
-const { albert_scraper } = require("./scraper")
-app.use(cors())
+const cors = require('cors');
+const {albert_scraper} = require('./scraper');
+app.use(cors());
 // use the morgan middleware to log all incoming http requests
-app.use(morgan("dev")) // morgan has a few logging default styles - dev is a nice concise color-coded style
+app.use(morgan('dev')); // morgan has a few logging default styles - dev is a nice concise color-coded style
 
 // use the bodyparser middleware to parse any data included in a request
-app.use(express.json()) // decode JSON-formatted incoming POST data
-app.use(express.urlencoded({ extended: true })) // decode url-encoded incoming POST data
+app.use(express.json()); // decode JSON-formatted incoming POST data
+app.use(express.urlencoded({extended: true})); // decode url-encoded incoming POST data
 
 // make 'public' directory publicly readable with static content
-app.use("/static", express.static("public"))
+app.use('/static', express.static('public'));
 
 
-app.get("/", (req, res) => {
-    res.send("bye!")
-})
-app.get("/html-example", (req, res) => {
-  res.sendFile("/public/some-page.html", { root: __dirname })
-})
+app.get('/', (req, res) => {
+  res.send('bye!');
+});
+app.get('/html-example', (req, res) => {
+  res.sendFile('/public/some-page.html', {root: __dirname});
+});
 
 // route for HTTP GET requests to /json-example
-app.get("/json-example", (req, res) => {
+app.get('/json-example', (req, res) => {
   // assemble an object with the data we want to send
   const body = {
-    title: "Hello!",
-    heading: "Hello!",
-    message: "Welcome to this JSON document, served up by Express",
-    //imagePath: "/static/images/donkey.jpg",
-  }
+    title: 'Hello!',
+    heading: 'Hello!',
+    message: 'Welcome to this JSON document, served up by Express',
+    // imagePath: "/static/images/donkey.jpg",
+  };
 
   // send the response as JSON to the client
-  res.json(body)
-  //res.status(200).json({ok:true})
-})
+  res.json(body);
+  // res.status(200).json({ok:true})
+});
 
 
-let email
-let position
-let number
+let email;
+let position;
+let number;
 
-app.post("/home_login", (req, res) => {
-  email = req.body.email
-  position = req.body.position
-  number = req.body.number
+app.post('/home_login', (req, res) => {
+  email = req.body.email;
+  position = req.body.position;
+  number = req.body.number;
   // const email = req.body.email
   // const position = req.body.position
   // const number = req.body.number
   // now do something amazing with this data...
   // ... then send a response of some kind
-  res.status(200).json({ok:true})
-  //res.send("hello")
+  res.status(200).json({ok: true});
+  // res.send("hello")
   console.log(email);
-})
+});
 
-app.get("/home_login", (req, res) => {
+app.get('/home_login', (req, res) => {
   // const email = req.body.email
   // const position = req.body.position
   // const number = req.body.number
   // now do something amazing with this data...
   // ... then send a response of some kind
-  res.send("hi")
-  //res.status(200).json({ok:true})
-})
+  res.send('hi');
+  // res.status(200).json({ok:true})
+});
 
-app.get("/class_modules", (req,res, next) => {
+app.get('/class_modules', (req, res, next) => {
   // use axios to make a request to an API for our class history data
   axios
-    .get("https://my.api.mockaroo.com/search_history.json?key=7fa4d720")
-    .then(apiResponse => res.json(apiResponse.data)) // pass data along directly to client
-    .catch(err => next(err)) // pass any errors to express
-  //res.status(200).json({ok:true})
-})
+      .get('https://my.api.mockaroo.com/search_history.json?key=7fa4d720')
+      .then((apiResponse) => res.json(apiResponse.data)) // pass data along directly to client
+      .catch((err) => next(err)); // pass any errors to express
+  // res.status(200).json({ok:true})
+});
 
 const calcProbGetIn = (position, number) => {
   // console.log(position, number)
   // need another function to pull class size based on the class number
   // need another function or algorithm to calculate the probability
-  return (100 - position) / 100 // this is a dummy return
-}
-app.get("/prof_scraper",(req,res)=>{
-  //scraper.prof_scraper();
-  scraper.prof_scraper("Amos Bloomberg","New York University");
-  res.send("scraper site");
- // res.status(200).json({ok:true})
-})
-app.get("/albert_scraper",(req,res)=>{
+  return (100 - position) / 100; // this is a dummy return
+};
+app.get('/prof_scraper', (req, res)=>{
+  // scraper.prof_scraper();
+  scraper.prof_scraper('Amos Bloomberg', 'New York University');
+  res.send('scraper site');
+  // res.status(200).json({ok:true})
+});
+app.get('/albert_scraper', (req, res)=>{
   scraper.albert_scraper();
-  res.send("albert_scraper");
-  //res.status(200).json({ok:true})
-  
-})
-app.get("/results", (req, res) => {
+  res.send('albert_scraper');
+  // res.status(200).json({ok:true})
+});
+app.get('/results', (req, res) => {
   scraper.albert_scraper();
   // call some function that takes email, position, number that the user entered on the home page
   // this function returns the probability that the student gets into the class
   // console.log( calcProbGetIn(position, number) )
-  const probGetIn = (calcProbGetIn(position, number) * 100).toString()
+  const probGetIn = (calcProbGetIn(position, number) * 100).toString();
   // console.log( probGetIn )
-  res.send(probGetIn) // we have to send a string here so we convert the probGetIn type to string above
-  //res.status(200).json({ok:true})
-})
+  res.send(probGetIn); // we have to send a string here so we convert the probGetIn type to string above
+  // res.status(200).json({ok:true})
+});
 
 
-app.get("/class_info", (req,res, next) => {
+app.get('/class_info', (req, res, next) => {
   scraper.albert_scraper();
   // use axios to make a request to an API for our class info data
   axios
-    .get("https://my.api.mockaroo.com/class_prof_info.json?key=01e62b90")
-    .then(apiResponse => res.json(apiResponse.data)) // pass data along directly to client
-    .catch(err => next(err)) // pass any errors to express
-    //res.status(200).json({ok:true})
-})
+      .get('https://my.api.mockaroo.com/class_prof_info.json?key=01e62b90')
+      .then((apiResponse) => res.json(apiResponse.data)) // pass data along directly to client
+      .catch((err) => next(err)); // pass any errors to express
+  // res.status(200).json({ok:true})
+});
 
-app.post("/login_logout", (req, res) => {
-  const email = req.body.email
-  const password = req.body.your_password
-  res.status(200).json({ok:true})
+app.post('/login_logout', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.your_password;
+  res.status(200).json({ok: true});
   console.log(email);
-  res.status(200).json({ok:true})
-})
+  res.status(200).json({ok: true});
+});
 
-app.get("/login_logout", (req, res) => {
-  const email = req.body.email
-  const password = req.body.your_password
-  res.send("hey there")
-  //res.status(200).json({ok:true})
-})
+app.get('/login_logout', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.your_password;
+  res.send('hey there');
+  // res.status(200).json({ok:true})
+});
 
-app.get("/prof_info", (req,res, next) => {
-    // use axios to make a request to an API for our class info data
-    scraper.prof_scraper("Amos Bloomberg","New York University")
-    axios
-      .get("https://my.api.mockaroo.com/professor.json?key=2f789220")
-      .then(apiResponse => res.json(apiResponse.data)) // pass data along directly to client
-      .catch(err => next(err)) // pass any errors to express
+app.get('/prof_info', (req, res, next) => {
+  // use axios to make a request to an API for our class info data
+  scraper.prof_scraper('Amos Bloomberg', 'New York University');
+  axios
+      .get('https://my.api.mockaroo.com/professor.json?key=2f789220')
+      .then((apiResponse) => res.json(apiResponse.data)) // pass data along directly to client
+      .catch((err) => next(err)); // pass any errors to express
 
-    //res.status(200).json({ok:true})
-}) 
+  // res.status(200).json({ok:true})
+});
 
 app.calcProbGetIn = calcProbGetIn;
 module.exports = app;
--- back-end/test.js
+++ back-end/test.js
@@ -1,39 +1,38 @@
-const axios=require('axios')
-const mocha=require('mocha')
-const chai=require('chai')
-const assert=require('assert')
-const scraper=require('./scraper')
-const app=require('./app')
-
+const axios=require('axios');
+const mocha=require('mocha');
+const chai=require('chai');
+const assert=require('assert');
+const scraper=require('./scraper');
+const app=require('./app');
 
 
 // dummy example
 describe('Array', function() {
-    describe('#indexOf()', function() {
-      it('should return -1 when the value is not present', function() {
-        assert.equal([1, 2, 3].indexOf(4), -1);
-      });
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
     });
   });
+});
 
 // test the dummy probability calculation for now
 describe('Calculate Probability of Getting Into Course', function() {
-    it('should return (100-position)/100 for now', function() {
-      assert.strictEqual(app.calcProbGetIn(7,100), .93);
-    });
+  it('should return (100-position)/100 for now', function() {
+    assert.strictEqual(app.calcProbGetIn(7, 100), .93);
+  });
 });
 
-describe('Scraping function for professors',function(){
+describe('Scraping function for professors', function() {
   this.timeout(30000);
-  it("should return(quality 3.9, difficulty 3.2, number of ratings 61, would take again 66%)",async function(){
+  it('should return(quality 3.9, difficulty 3.2, number of ratings 61, would take again 66%)', async function() {
     this.timeout(30000);
-    //setTimeout(done,30000);
-    let res= await scraper.prof_scraper("Amos Bloomberg","New York University");
-    
-    assert.equal(res.q,'3.9');
-    assert.equal(res.d,'3.2');
-    assert.equal(res.r,'61');
-    assert.equal(res.t,"66%");
+    // setTimeout(done,30000);
+    const res= await scraper.prof_scraper('Amos Bloomberg', 'New York University');
+
+    assert.equal(res.q, '3.9');
+    assert.equal(res.d, '3.2');
+    assert.equal(res.r, '61');
+    assert.equal(res.t, '66%');
   });
 });
 
