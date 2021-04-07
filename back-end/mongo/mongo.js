7c88552eba7a6c8d0608b0bf51e4a23335fad58f
Sophie Pan <sp5590@stern.nyu.edu>
2021-04-06 20:01:32 -0400

    added some stuff to mongo.js and LoginLogout.js in front end

--- back-end/mongo/mongo.js
+++ back-end/mongo/mongo.js
@@ -5,10 +5,14 @@ const router = express.Router();
 
 const pwd=process.env.mongoPWD;
 const user=process.env.mongoUSER;
+let bodyParser = require('body-parser');
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
 //copy pasted from mongoDB
 const mongoScript=async()=>{
 
-    const uri = `mongodb+srv://whDev:${pwd}@clusterwh.bhiht.mongodb.net/sample_airbnb?retryWrites=true&w=majority`;
+    const uri = `mongodb+srv://${user}:${pwd}@clusterwh.bhiht.mongodb.net/sample_airbnb?retryWrites=true&w=majority`;
     const client = await new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     await client.connect(err => {
         
@@ -25,12 +29,47 @@ const mongoScript=async()=>{
     });
     client.close();
 };
-const mongoInster=async(mongoURL)=>{
+
+mongoose.Promise = global.Promise;
+
+const mongoInsert=async(mongoURL)=>{
+    
+    mongoose.connect(mongoURL)
+        .catch(err => {
+            res.status(400).send("unable to connect to database");
+        })
 
 };
+
+let userAccountSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+let User = mongoose.model("User", userAccountSchema);
+
+router.get("/add_user_account", (req, res) => {
+    res.send("add user account");
+});
+
+router.post("/add_user_account", (req, res) => {
+    let myUser = new User(req.body);
+    myUser.save()
+        .then(item => {
+            res.send("item saved to database");
+        })
+        .catch(err => {
+            res.status(400).send("unable to save to database");
+        })
+});
+
 router.get("/",(req,res)=>{
 
+    const uri = `mongodb+srv://${user}:${pwd}@clusterwh.bhiht.mongodb.net/sample_airbnb?retryWrites=true&w=majority`;
+    mongoInsert(uri);
+
     res.send('mongo_router');
+
 });
 
 module.exports={
