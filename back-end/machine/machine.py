fecc8604d4c18024c058f49901486de726693185
Sophie Pan <sp5590@stern.nyu.edu>
2021-04-10 18:55:54 -0400

    added all columns to dataframe

--- back-end/machine/machine.py
+++ back-end/machine/machine.py
@@ -10,20 +10,27 @@ value = input()
 # print("Input value:", value)
 
 jsonval = json.loads(value) # array of dictionaries
-# print("JSON Value:", jsonval[0]['courseNum'])
+# print("JSON Value:", jsonval[0])
 
 courseNums = []
+courseSizes = []
+waitlistSizes = []
 for i in jsonval:
     courseNums.append(i['courseNum'])
-    
-print(courseNums)
+    courseSizes.append(i['courseSizes'])
+    waitlistSizes.append(i['waitlistSizes'])
 
-columns = ['Course Number', 'Class Size', 'Waitlist Size']
+
+columns = ['Course Number', 'Course Size', 'Waitlist Size']
 df = pd.DataFrame(columns = columns)
 df['Course Number'] = courseNums
+df['Course Size'] = courseSizes
+df['Waitlist Size'] = waitlistSizes
 
-print(df)
+# this works but it will print ... 
+# for df['Course Size] and df['Waitlist Size'] since they are arrays
+# print(df) 
 
-# df = pd.read_json(value, 'columns')
+
 
 # def prosterity(classSize,waitListPos):
