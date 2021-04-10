04b2a74c1b2115e8b17cc7d7593be3330beaeaf1
Sophie Pan <sp5590@stern.nyu.edu>
2021-04-10 18:44:15 -0400

    created pandas dataframe

--- back-end/machine/machine.py
+++ back-end/machine/machine.py
@@ -1,7 +1,29 @@
 import numpy as np
 import pandas as pd
-import scikit-learn as sl
+import sklearn as sl
 from faker.providers.person.en import Provider
+import json
 
 print('Machine.py triggered')
-def prosterity(classSize,waitListPos):
+
+value = input()
+# print("Input value:", value)
+
+jsonval = json.loads(value) # array of dictionaries
+# print("JSON Value:", jsonval[0]['courseNum'])
+
+courseNums = []
+for i in jsonval:
+    courseNums.append(i['courseNum'])
+    
+print(courseNums)
+
+columns = ['Course Number', 'Class Size', 'Waitlist Size']
+df = pd.DataFrame(columns = columns)
+df['Course Number'] = courseNums
+
+print(df)
+
+# df = pd.read_json(value, 'columns')
+
+# def prosterity(classSize,waitListPos):
