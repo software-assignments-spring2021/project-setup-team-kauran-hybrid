6983e82af5bc32538dab8cad4740f74776047aa9
evlzhang <zyh991001@gmail.com>
2021-04-10 23:15:59 -0400

    Added linear regression but for wrong x and y

--- back-end/machine/machine.py
+++ back-end/machine/machine.py
@@ -1,8 +1,10 @@
 import numpy as np
 import pandas as pd
 import sklearn as sl
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import KFold
+from sklearn import preprocessing
 import matplotlib.pyplot as plt
-from faker.providers.person.en import Provider
 import json
 
 print('Machine.py triggered')
@@ -13,40 +15,47 @@ value = input()
 jsonval = json.loads(value) # array of dictionaries
 # print("JSON Value:", jsonval[0])
 
+def makeDF():
+    courseNums = []
+    courseSizes = []
+    waitlistSizes = []
+    '''
+    for i in jsonval:
+        courseNums.append(i['courseNum'])
+        courseSizes.append(i['courseSizes'])
+        waitlistSizes.append(i['waitlistSizes'])
+    '''
+    for i in jsonval:
+        courseNum=i['courseNum']
+        for j in range(len(i['courseSizes'])):
+            courseNums.append(courseNum)
+            courseSizes.append(i['courseSizes'][j])
+            waitlistSizes.append(i['waitlistSizes'][j])
 
-courseNums = []
-courseSizes = []
-waitlistSizes = []
-'''
-for i in jsonval:
-    courseNums.append(i['courseNum'])
-    courseSizes.append(i['courseSizes'])
-    waitlistSizes.append(i['waitlistSizes'])
-'''
-for i in jsonval:
-    courseNum=i['courseNum']
-    for j in range(len(i['courseSizes'])):
-        courseNums.append(courseNum)
-        courseSizes.append(i['courseSizes'][j])
-        waitlistSizes.append(i['waitlistSizes'][j])
+    columns = ['CourseNumber', 'CourseSize', 'WaitlistSize']
+    df = pd.DataFrame(columns = columns)
+    df['CourseNumber'] = courseNums
+    df['CourseSize'] = courseSizes
+    df['WaitlistSize'] = waitlistSizes
 
+    df.set_index('CourseNumber',inplace=True,drop=True)
+    df=df.astype(int)
+    print("Exists null data? :",df.isnull().values.any())
+    return df
+    
+df=makeDF()
 
+def linReg(DataFrame:df):
+    x=pd.DataFrame(df['CourseSize'])
+    y=pd.DataFrame(df['WaitlistSize'])
+    model=LinearRegression()
+    scores=[]
+    kfold=KFold(n_splits=3,shuffle=True,random_state=42)
+    for i, (train, test) in enumerate(kfold.split(x,y)):
+        model.fit(x.iloc[train,:], y.iloc[train,:])
+        score=model.score(x.iloc[test,:],y.iloc[test,:])
+        scores.append(score)
+    print("Scores: ", scores)
 
-columns = ['Course Number', 'Course Size', 'Waitlist Size']
-df = pd.DataFrame(columns = columns)
-df['Course Number'] = courseNums
-df['Course Size'] = courseSizes
-df['Waitlist Size'] = waitlistSizes
-
-
-
-
-df.set_index('Course Number',inplace=True,drop=True)
-
-#df.plot()
-
-print(df)
-
-
-
+linReg(df)
 # def prosterity(classSize,waitListPos):
