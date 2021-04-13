ba279c8ab8bbb5fa2d12af83ec9a117eeff39ded
evlzhang <zyh991001@gmail.com>
2021-04-13 00:36:45 -0400

    Successfully fixed signup and login with extra parameters!

--- back-end/login_logout.js
+++ back-end/login_logout.js
@@ -8,7 +8,7 @@ const passport = require("passport");
 const { check, validationResult } = require("express-validator");
 const dotenv = require("dotenv");
 const whModels = require("./mongo/wh_models.js")
-const userAccounts = whModels.userAccounts;
+//const userAccounts = whModels.userAccounts;
 const LocalStrategy = require("passport-local").Strategy;
 const JwtStrategy = require("passport-jwt").Strategy;
 const { ExtractJwt } = require("passport-jwt");
@@ -65,45 +65,32 @@ const loginSuccessChecker = (username, password) => {
 //   })
 // )
 
-passport.use('login', new LocalStrategy({usernameField:'username'},(username, password, done) => {
+passport.use('login', new LocalStrategy({usernameField:'username', passwordField: 'password', passReqToCallback: true},(req,username, password, done) => {
   const uri = `mongodb+srv://${mongoUser}:${mongoPwd}@clusterwh.bhiht.mongodb.net/user_accounts?retryWrites=true&w=majority`;
-  mongoose.connect(uri,{useNewUrlParser:true,useUnifiedTopology:true});
+  const link = mongoose.createConnection(uri,{useNewUrlParser:true,useUnifiedTopology:true});
+  const userAccounts = link.model('userAccounts', whModels.userAccountSchema)
   userAccounts.findOne({ username: username}, (err, user) => {
       if (err) throw err;
       if(!user){
-        
-        // const newUser=new userAccounts({username,password});
-        // console.log(newUser);
-        // bcrypt.genSalt(10,(err,salt)=>{
-        //   if(err) throw err;
-        //   bcrypt.hash(newUser.password,salt,(err,hash)=>{
-        //     if(err) throw err;
-        //     newUser.password=hash;
-        //     newUser
-        //       .save()
-        //       .then(user=>{
-        //         return done(null,user);
-        //       })
-        //       .catch(err=>{
-        //         return done(null,false,{message:err});
-        //       });
-        //   });
-        // });
         console.log("Account Doesn't Exist!");
         return done(null,false,{message:"Account Does Not Exist"});
         
       }
       if(user){
-        console.log(password,user.password);
+        //console.log(password,user.password);
         bcrypt.compare(password,user.password,(err,isMatch)=>{
           if(err) throw err;
           if(isMatch){
             console.log("Matches");
+            if(req.body.number){
+              console.log('Extra params');
+              user.save({courseNum:user.courseNum.push(req.body.number),waitlistPos:user.waitlistPos.push(req.body.position)});
+            }
             return done(null,user,{message:"Matches"});
           }
           else{
-            console.log("Wrong password");
-            return done(null,false,{message:"wrong password"});
+            console.log("Wrong Password");
+            return done(null,false,{message:"Wrong Password"});
           }
         });
       }
@@ -115,8 +102,8 @@ passport.use('signup',
 new LocalStrategy({usernameField:'username', passwordField: 'password', passReqToCallback: true},
 (req,username,password,done) => {
 
-  // let number = req.body.number
-  // let position = req.body.position
+  let number = req.body.number
+  let position = req.body.position
   const uri = `mongodb+srv://${mongoUser}:${mongoPwd}@clusterwh.bhiht.mongodb.net/user_accounts?retryWrites=true&w=majority`;
   const link = mongoose.createConnection(uri,{useNewUrlParser:true,useUnifiedTopology:true});
   const userAccounts = link.model('userAccounts', whModels.userAccountSchema)
@@ -125,15 +112,18 @@ new LocalStrategy({usernameField:'username', passwordField: 'password', passReqT
       if (err) throw err;
       if(!user){
         
-        const newUser=new userAccounts({username,password});
+        const newUser=new userAccounts({username:username,password:password,courseNum:number,waitlistPos:position});
         
         // console.log(newUser);
         bcrypt.genSalt(10,(err,salt)=>{
-          // console.log(username,password); 
+           
           if(err) throw err;
           bcrypt.hash(newUser.password,salt,(err,hash)=>{
             if(err) throw err;
+            console.log(number,position);
             newUser.password=hash;
+            // newUser.courseNum=number;
+            // newUser.waitlistPos=position;
             newUser
               .save()
               .then(user=>{
@@ -225,13 +215,6 @@ router.post('/login',(req,res,next)=>{
       return res.status(200)//.json({success: 'logged in '});
     });
   })(req,res,next);
-  
-  if(req.body.number) {
-    console.log('something')
-    const uri = `mongodb+srv://${mongoUser}:${mongoPwd}@clusterwh.bhiht.mongodb.net/user_accounts?retryWrites=true&w=majority`;
-    mongo.mongoSaveUserHistory(uri,req.body.username,null,req.body.number,req.body.position);
-    
-  }
  });
 
 module.exports = {
